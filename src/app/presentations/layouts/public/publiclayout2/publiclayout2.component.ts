70a91972e7d440758390117dc60ae83566d59192
kinley wangyel <kwangyel@gmail.com>
2024-05-03 13:10:48 +0600

    home2

--- src/app/presentations/layouts/public/publiclayout2/publiclayout2.component.ts
+++ src/app/presentations/layouts/public/publiclayout2/publiclayout2.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PublicNavbar2Component } from '../public-navbar2/public-navbar2.component';
+import { PublicFooterComponent } from '../public-footer/public-footer.component';
 
 @Component({
     selector: 'app-publiclayout2',
     standalone: true,
-    imports: [RouterModule, PublicNavbar2Component],
+    imports: [RouterModule, PublicNavbar2Component, PublicFooterComponent],
     templateUrl: './publiclayout2.component.html',
     styleUrls: ['./publiclayout2.component.scss'],
 })
--- src/app/presentations/public/home/publichom2/publichom2.component.ts
+++ src/app/presentations/public/home/publichom2/publichom2.component.ts
@@ -1,15 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipsModule } from 'primeng/chips';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessagesModule } from 'primeng/messages';
+import { StyleClassModule } from 'primeng/styleclass';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
     selector: 'app-publichom2',
     standalone: true,
-    imports: [RouterModule],
+    imports: [
+        RouterModule,
+        FormsModule,
+        InputTextModule,
+        CheckboxModule,
+        DropdownModule,
+        ButtonModule,
+        ChipsModule,
+        MessagesModule,
+        ToastModule,
+        CardModule,
+        TableModule,
+        DividerModule,
+        StyleClassModule,
+    ],
     templateUrl: './publichom2.component.html',
     styleUrls: ['./publichom2.component.scss'],
 })
 export class Publichom2Component implements OnInit {
+    searchInTitle:boolean;
+    documentTypes
+    searchDocumentType
+    searchKeywords
+
     constructor() {}
 
     ngOnInit() {}
+    searchForLegislations(){}
+    toggleCheckbox(content){}
 }
